63f3965ccc8acad95d2316fdd3ad7fd563a20000
casesolved-co-uk <richard@casesolved.co.uk>
2021-02-16 04:53:20 +0000

    fix: GBP setup fix: update db with currency names and money_in_words. Also fix GBP and USD chore: sider issue chore: ignore nano backups fix: added code to normalise currency data fix: overzealous name setting fix: Update and normalise the majority of Currency data fix: overwrite currency info with latest if it exists and remove orphans chore: sider issue fix: Resolve comments feat: add field to link Country to Currency so data can be dumped back out to fixture fix: change country currency db field to align with fixture fix: update and normalise currency data fix: try to make currency list more readable feat: add function for dumping Country and Currency data from db to fixture feat: improve country and currency import fix: bug in orphan currency detection feat: Add dump fixture button on Country page fix: add permissions check to dump fixtures operation fix: ui tweaks feat: add delete permission to System Manager fix: ESLint issues fix: sider and user input fail on build fix: test cases fix: add patch

--- frappe/geo/country_info.py
+++ frappe/geo/country_info.py
@@ -19,6 +19,10 @@ def get_country_info(country=None):
 		data.date_format = "dd-mm-yyyy"
 	if "time_format" not in data:
 		data.time_format = "HH:mm:ss"
+	if "currency" not in data:
+		data.currency = ""
+	if "number_format" in data:
+		sections, data.currency_fraction_units, data.smallest_currency_fraction_value = number_format_info(data.get("number_format"))
 
 	return data
 
@@ -28,6 +32,13 @@ def get_all():
 		all_data = json.loads(local_info.read())
 	return all_data
 
+def number_format_info(format):
+	import re
+	nfsplit = re.findall("#+", format)
+	sections = len(nfsplit)
+	decimal_places = 0 if sections <= 2 else len(nfsplit[-1])
+	fraction_units = int("1" + "0" * decimal_places)
+	return sections, fraction_units, 1.0 / fraction_units
 
 @frappe.whitelist(allow_guest=True)
 def get_country_timezone_info():
@@ -58,8 +69,35 @@ def get_translated_countries():
 
 	return translated_dict
 
+def normalise():
+	"Manual helper function to keep the fixture up-to-date & normalised"
+	all_data = get_all()
+	for country in all_data:
+		all_data[country] = get_country_info(country)
+
+	with open(os.path.join(os.path.dirname(__file__), "country_info.json"), "w") as local_info:
+		json.dump(all_data, local_info, indent=1, sort_keys=True)
+
+@frappe.whitelist()
+def dump_fixture_from_db():
+	"Update the repo fixture from the content of the db"
+	frappe.only_for(["Administrator", "System Manager"])
+	with open(os.path.join(os.path.dirname(__file__), "country_info.json"), "w") as fp:
+		ctlist = frappe.get_all("Country", fields=["name","code","date_format","time_format","time_zones","currency"], order_by="name", as_list=False)
+		fixture = {ct.pop("name"): ct for ct in ctlist}
+		cylist = frappe.get_all("Currency", fields=["name","unit_name as currency_name","fraction as currency_fraction",
+			"fraction_units as currency_fraction_units","smallest_currency_fraction_value","symbol as currency_symbol","number_format"], order_by="name", as_list=False)
+		cys = {cy.pop("name"): cy for cy in cylist}
+		for ctn, ct in fixture.items():
+			ct["timezones"] = ct.pop("time_zones").split("\n")
+			if ct["currency"]:
+				ct.update(cys[ct["currency"]])
+			else:
+				ct.pop("currency")
+		json.dump(fixture, fp, indent=1, sort_keys=True)
 
 def update():
+	"Manual helper function to complete the fixture number formats"
 	with open(os.path.join(os.path.dirname(__file__), "currency_info.json")) as nformats:
 		nformats = json.loads(nformats.read())
 
---
+++ frappe/geo/doctype/currency/currency_list.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Frappe Technologies and contributors
+// For license information, please see license.txt
+frappe.listview_settings['Currency'] = {
+	hide_name_column: true,
+	filters: [
+		['enabled', '=', 1]
+	]
+};
