f242249436aee1e3bb3d37150f2ea89f83feaefe
Richard Case <richard@casesolved.co.uk>
2022-11-09 23:42:50 +0000

    feat: wkhtmltopdf logging fix: pdf logging in case of exception

--- frappe/utils/pdf.py
+++ frappe/utils/pdf.py
@@ -19,6 +19,7 @@ from frappe import _
 from frappe.core.doctype.file.utils import find_file_by_url
 from frappe.utils import cstr, scrub_urls
 from frappe.utils.jinja_globals import bundled_asset, is_rtl
+from frappe.utils.logger import pipe_to_log
 
 PDF_CONTENT_ERRORS = [
 	"ContentNotFoundError",
@@ -27,11 +28,13 @@ PDF_CONTENT_ERRORS = [
 	"RemoteHostClosedError",
 ]
 
+logger = frappe.logger("wkhtmltopdf", max_size=100000, file_count=3)
+logger.setLevel("INFO")
 
 def get_pdf(html, options=None, output: PdfWriter | None = None):
+	global logger
 	html = scrub_urls(html)
 	html, options = prepare_options(html, options)
-
 	options.update({"disable-javascript": "", "disable-local-file-access": ""})
 
 	filedata = ""
@@ -39,8 +42,13 @@ def get_pdf(html, options=None, output: PdfWriter | None = None):
 		options.update({"disable-smart-shrinking": ""})
 
 	try:
+		# wkhtmltopdf writes the pdf to stdout and errors to stderr
+		# pdfkit v1.0.0 writes the pdf to file or returns it
+		# stderr is written to sys.stdout if verbose=True is supplied
 		# Set filename property to false, so no file is actually created
-		filedata = pdfkit.from_string(html, options=options or {}, verbose=True)
+		# defaults to redirecting stdout
+		with pipe_to_log(logger.info):
+			filedata = pdfkit.from_string(html, False, options=options or {}, verbose=True)
 
 		# create in-memory binary streams from filedata and create a PdfReader object
 		reader = PdfReader(io.BytesIO(filedata))
@@ -97,7 +105,6 @@ def prepare_options(html, options):
 			"print-media-type": None,
 			"background": None,
 			"images": None,
-			"quiet": None,
 			# 'no-outline': None,
 			"encoding": "UTF-8",
 			# 'load-error-handling': 'ignore'
